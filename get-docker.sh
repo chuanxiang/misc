afb7c73045780888fa1d534ef12445b618563caf
chuanxiang <chuanxiang@gmail.com>
2019-07-13 16:54:55 +0800

    add get-docker.sh

---
+++ get-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# no need to run as root
+sudo groupadd docker
+sudo usermod -aG docker $USER
+echo "please logout first!"
